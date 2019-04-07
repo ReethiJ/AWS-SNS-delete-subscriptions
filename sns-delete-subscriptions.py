56cfe252d8b88b949eb7b3245c4d1ff74b9fad4d
Reethi Joseph <reethi@amazon.com>
2019-04-07 14:29:31 +0530

    second commit

--- sns-delete-subscriptions.py
+++ sns-delete-subscriptions.py
@@ -3,16 +3,16 @@ import boto3
 client = boto3.client('sns')
 
 def delete_sqs_subscription():
-    response = client.list_subscriptions_by_topic(TopicArn='arn:aws:sns:<region>:<ACCOUNT-NUMBER>:<SNS-TOPIC>')
+    sns_list_response = client.list_subscriptions_by_topic(TopicArn='arn:aws:sns:<region>:<ACCOUNT-NUMBER>:<SNS-TOPIC>')
     while True:
-        for subscArn in response['Subscriptions']:
+        for subscArn in sns_list_response['Subscriptions']:
             if "<SOME-PREFIX>" in subscArn['Endpoint']:
                 print("Found endpoint ",subscArn['Endpoint'])
                 print("Deleting Subscription",subscArn['SubscriptionArn'])
-                response = client.unsubscribe(SubscriptionArn=subscArn['SubscriptionArn'])
-                print(response)
-        if 'NextToken' in response:
-            response = client.list_subscriptions_by_topic(TopicArn='arn:aws:sns:<region>:<ACCOUNT-NUMBER>:<SNS-TOPIC>',  NextToken=response['NextToken'])
+                unsubs_response = client.unsubscribe(SubscriptionArn=subscArn['SubscriptionArn'])
+                print(unsubs_response)
+        if 'NextToken' in sns_list_response:
+            sns_list_response = client.list_subscriptions_by_topic(TopicArn='arn:aws:sns:<region>:<ACCOUNT-NUMBER>:<SNS-TOPIC>',  NextToken=sns_list_response['NextToken'])
         else:
             break
 
