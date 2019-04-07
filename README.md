5d7c6cae1d1f9760b68f62807dc6f2ae79064c1b
Reethi Joseph <reethi@amazon.com>
2019-04-07 14:24:20 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AWS-SNS-delete-subscriptions
+# AWS-SNS-delete-subscription 
+
+Python script to remove SNS subscriptions based on the Endpoint value. This can be particularly useful in removing particular SQS subscriptions that begin with a prefix.
--- sns-delete-subscriptions.py
+++ sns-delete-subscriptions.py
@@ -6,7 +6,7 @@ def delete_sqs_subscription():
     response = client.list_subscriptions_by_topic(TopicArn='arn:aws:sns:<region>:<ACCOUNT-NUMBER>:<SNS-TOPIC>')
     while True:
         for subscArn in response['Subscriptions']:
-            if "feed-" in subscArn['Endpoint']:
+            if "<SOME-PREFIX>" in subscArn['Endpoint']:
                 print("Found endpoint ",subscArn['Endpoint'])
                 print("Deleting Subscription",subscArn['SubscriptionArn'])
                 response = client.unsubscribe(SubscriptionArn=subscArn['SubscriptionArn'])
